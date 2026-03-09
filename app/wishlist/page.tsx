08c4e5389e2d3a6d6c0c952427064ca2f0dd2353
durimfetahaj <131853939+durimfetahaj@users.noreply.github.com>
2026-03-09 19:52:55 +0100

    added wish-list ui

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,8 +1,78 @@
+import { Button } from "@/components/ui/button";
+import { ShoppingBag, ShoppingCart, X } from "lucide-react";
+
+const wishlistItems = [
+  {
+    id: 1,
+    name: "Wireless Noise-Canceling Headphones",
+    category: "Electronics",
+    price: "$29.99",
+    addedAt: "2024-06-01T10:00:00Z",
+    image:
+      "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/ecommerce/electronics/Modern-White-Headphones-1.png",
+  },
+  {
+    id: 2,
+    name: "Premium Leather Crossbody Bag",
+    category: "Clothing",
+    price: "$49.99",
+    addedAt: "2024-06-02T14:30:00Z",
+    image:
+      "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/ecommerce/clothes/maroon-leather-handbag.png",
+  },
+];
+
 export default function WishlistPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center ">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-8xl">Wishlist Page</h1>
+    <div className="flex min-h-screen">
+      <main className="flex min-h-screen w-full flex-col items-center gap-20 py-32 bg-white dark:bg-black sm:items-start">
+        <div className="flex items-center justify-between w-full">
+          <p className="text-lg">Wishlist ({wishlistItems.length})</p>
+          <Button className="cursor-pointer py-5 px-4" variant="default">
+            Move All To Bag
+          </Button>
+        </div>
+
+        <div className="border w-full rounded-lg flex flex-col">
+          {wishlistItems.map((item) => (
+            <div
+              key={item.id}
+              className="flex items-center justify-between space-x-4 p-5 border-b last:border-b-0"
+            >
+              <div className="flex gap-5">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-30 h-30 object-cover rounded-lg"
+                />
+                <div className="flex flex-col justify-between gap-2 flex-1">
+                  <div>
+                    <h3 className="text-lg font-semibold">{item.name}</h3>
+                    <p className="text-sm text-gray-500">{item.category}</p>
+                  </div>
+                  <p className="text-xl font-bold">{item.price}</p>
+                  <p className="text-xs text-gray-400">
+                    Added at: {new Date(item.addedAt).toLocaleDateString()}
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex flex-col justify-between gap-2 h-full">
+                <div className="flex justify-end">
+                  <Button className="cursor-pointer">
+                    <X />
+                  </Button>
+                </div>
+                <div className="flex">
+                  <Button className="flex items-center gap-2 cursor-pointer">
+                    <ShoppingCart />
+                    Add to bag
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
