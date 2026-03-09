935dba648e945991ea93c09ebb5b6046525c3b84
durimfetahaj <131853939+durimfetahaj@users.noreply.github.com>
2026-03-09 19:16:01 +0100

    added about,contact,wishtlist and,cart pages

---
+++ app/wishlist/page.tsx
@@ -0,0 +1,9 @@
+export default function WishlistPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center ">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-8xl">Wishlist Page</h1>
+      </main>
+    </div>
+  );
+}
